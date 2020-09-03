fbea37634469de001f9489cd65fd70620fe4ea0b
Tyler Menezes <github@xanadu.ws>
2020-09-03 17:20:05 -0400

    Add booking page

---
+++ src/components/Calendly.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState, useRef } from 'react';
+import PropTypes from 'prop-types';
+import Box from '@codeday/topo/Atom/Box';
+
+export default function Calendly ({ slug, ...props }) {
+  const holder = useRef(null);
+  const [hasCalendlyLoaded, setHasCalendlyLoaded] = useState(false);
+
+  const typeOfWindow = typeof window; // For static analysis
+  const windowCalendly = typeOfWindow !== 'undefined' && window?.Calendly;
+
+  useEffect(() => {
+    if (window && !window.Calendly) {
+      const script = document.createElement('script');
+      script.src = 'https://calendly.com/assets/external/widget.js';
+      script.addEventListener('load', () => {
+        setHasCalendlyLoaded(true);
+      });
+      document.head.appendChild(script);
+
+      const link = document.createElement('link');
+      link.href = 'https://calendly.com/assets/external/widget.css';
+      link.rel = 'stylesheet';
+      document.body.appendChild(link);
+
+      return () => {
+        document.head.removeChild(script);
+        document.body.removeChild(link);
+      }
+    }
+  }, [typeOfWindow]);
+
+  useEffect(() => {
+    if (windowCalendly && holder) {
+      window.Calendly.initInlineWidget({
+        url: `https://calendly.com/${slug}`,
+        parentElement: holder.current,
+        prefill: {},
+        utm: {}
+      });
+    }
+  }, [windowCalendly, hasCalendlyLoaded, holder, slug]);
+
+  return (
+    <Box w="100%" h="50em" border="none" {...props} ref={holder} />
+  );
+}
+Calendly.propTypes = {
+  slug: PropTypes.string.isRequired,
+}
---
+++ src/pages/m/[slug].js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Spinner from '@codeday/topo/Atom/Spinner';
+import Content from '@codeday/topo/Molecule/Content';
+import Calendly from '../../components/Calendly';
+import Page from '../../components/Page';
+
+export default function CalendlyPage({ slug }) {
+  return (
+    <Page slug={`/m/${slug}`} title="Schedule Meeting">
+      <Content>
+        {!slug ? <Spinner /> : (
+          <Calendly mt={-8} slug={`${slug}?hide_event_type_details=1`} />
+        )}
+      </Content>
+    </Page>
+  );
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [],
+    fallback: true,
+  };
+}
+
+export async function getStaticProps({ params: { slug } }) {
+  return {
+    props: {
+      slug,
+    },
+    revalidate: 300,
+  };
+}
