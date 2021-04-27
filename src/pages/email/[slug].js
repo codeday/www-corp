4f1c125fcd1b3329b27adb8f5deee461322f90d6
Lola <lola@egherman.com>
2021-04-27 14:01:12 -0500

    add email list subscribe

---
+++ src/pages/email/[slug].js
@@ -0,0 +1,20 @@
+import React from 'react';
+import MailingListSubscribe from '@codeday/topo/Organism/MailingListSubscribe'
+import Text, { Heading } from '@codeday/topo/Atom/Text';
+import Content from '@codeday/topo/Molecule/Content';
+import { useRouter } from 'next/router';
+import Page from '../../components/Page';
+
+export default function Home() {
+  const { isFallback, query } = useRouter();
+
+  return (
+    <Page slug={`/email/${query.slug}`}>
+      <Content maxWidth="xl">
+        <Heading maxWidth="x1">Get notified of opportunities from CodeDay!</Heading>
+        <MailingListSubscribe maxWidth="x1" emailList={query.slug} />
+        <Text color="gray.300">(We won't spam you, pinky promise)</Text>
+      </Content>
+    </Page>
+  );
+}
