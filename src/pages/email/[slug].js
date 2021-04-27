332d673277803fa8c891fdf5c54efcc409e97bb4
Lola <lola@egherman.com>
2021-04-27 14:06:56 -0500

    minor text changes

--- src/pages/email/[slug].js
+++ src/pages/email/[slug].js
@@ -13,7 +13,10 @@ export default function Home() {
       <Content maxWidth="xl">
         <Heading maxWidth="x1">Get notified of opportunities from CodeDay!</Heading>
         <MailingListSubscribe maxWidth="x1" emailList={query.slug} />
-        <Text color="gray.300">(We won't spam you, pinky promise)</Text>
+        <Text color="gray.300">(We won't spam you, pinky promise!)
+          <br />
+          We don't use any tracking in our emails and you can unsubscribe whenever
+        </Text>
       </Content>
     </Page>
   );
