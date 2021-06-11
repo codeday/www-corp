df346017651e53aa057a7d5e35daee327a09c3e4
Tyler Menezes <tylermenezes@codeday.org>
2021-06-11 12:54:21 -0400

    Add event pages

---
+++ src/pages/e/[calendarId]/[eventId]/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import { NextSeo } from 'next-seo';
+import Content from '@codeday/topo/Molecule/Content';
+import Text, { Link } from '@codeday/topo/Atom/Text';
+import { apiFetch } from '@codeday/topo/utils';
+import Page from '../../../../components/Page';
+import Event from '../../../../components/EventInfo';
+import { EventByIdQuery } from './index.gql';
+
+export default function Home({ event }) {
+  const { id } = useRouter().query;
+
+  return (
+    <Page slug={`/e/${id}`} title={event.title}>
+      <NextSeo
+        description={event.description}
+      />
+      <Content mt={-8}>
+        <Event event={event} />
+      </Content>
+    </Page>
+  );
+}
+
+export async function getStaticPaths() {
+  return {
+    fallback: 'blocking',
+    paths: [],
+  };
+}
+
+export async function getStaticProps({ req, params: { calendarId, eventId } }) {
+  const resp = await apiFetch(EventByIdQuery, { id: eventId, calendarId });
+  return {
+    props: {
+      event: resp?.calendar?.event,
+    }
+  }
+}
