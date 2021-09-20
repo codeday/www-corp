d713e577233cd6ad189f440a3791752e5de0662b
Tyler Menezes <tylermenezes@codeday.org>
2021-09-20 12:08:47 -0400

    Add webinars to announcement box

--- src/components/Index/Announcement.js
+++ src/components/Index/Announcement.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { DateTime } from 'luxon';
 import Content from '@codeday/topo/Molecule/Content';
 import Box, { Grid } from '@codeday/topo/Atom/Box';
 import Text from '@codeday/topo/Atom/Text';
@@ -11,19 +12,44 @@ const fromIso = (s) => {
 }
 
 export default function Announcement(props) {
-  const { cms: { announcements } } = useQuery();
+  const { cms: { announcements }, announcementWebinar: { events } } = useQuery();
 
-  const sortedAnnouncements = (!announcements?.items || announcements.items.length === 0) ? null : (
+  const now = new Date();
+  const webinarAnnouncementEndDate = (new Date()).setDate(now.getDate()+2);
+
+  const sortedAnnouncements = (!announcements?.items || announcements.items.length === 0) ? [] : (
     announcements.items.sort((a, b) => fromIso(a.displayAt) > fromIso(b.displayAt) ? -1 : 1)
   );
 
-  if (!sortedAnnouncements || sortedAnnouncements.length === 0) return <></>;
-  const announcement = sortedAnnouncements[0];
+  const sortedEvents = (!events || events.length === 0) ? [] : (
+    events
+      .filter((e) => fromIso(e.start) < webinarAnnouncementEndDate && fromIso(e.end) >= now)
+      .sort((a, b) => fromIso(a.start) > fromIso(b.start))
+  );
+
+  let announcement;
+  if (sortedEvents.length > 0) {
+    const event = sortedEvents[0];
+    const start = DateTime.fromISO(event.start).toLocaleString({
+      day: 'numeric',
+      month: 'numeric',
+      hour: 'numeric',
+      minute: '2-digit'
+    });
+    announcement = {
+      link: event.metadata?.preregister ? event.location : `/e/${event.calendarId}/${event.id}`,
+      type: 'Upcoming Webinar',
+      oneline: `${event.title} (${start})`,
+      cta: 'Register',
+    };
+  } else if (sortedAnnouncements > 0) announcement = sortedAnnouncements[0];
+  else return <></>;
 
   const baseColor = {
     New: 'blue',
-    Improved: 'indigo',
-    Alert: 'red'
+    Improved: 'yellow',
+    Alert: 'red',
+    'Upcoming Webinar': 'indigo',
   }[announcement.type];
 
   return (
@@ -47,12 +73,12 @@ export default function Announcement(props) {
         >
           <Box>
             <Text mb={0} d="inline">
-                <Text as="span" bold d={{ base: 'inline', md: 'none' }}>New: </Text>
+                <Text as="span" bold d={{ base: 'none', md: 'inline' }}>{announcement.type}: </Text>
                 {announcement.oneline}
             </Text>
           </Box>
           <Box textAlign="right" display={{ base: 'none', md: 'block' }}>
-            <Button size="sm" variantColor={baseColor} mb={0}>Learn More</Button>
+            <Button size="sm" variantColor={baseColor} mb={0}>{announcement.cta || 'Learn More'}</Button>
           </Box>
         </Grid>
       </Box>
