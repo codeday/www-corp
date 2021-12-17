86a3842313de79d6e338753ac7f06b338a86a19a
Tyler Menezes <tylermenezes@codeday.org>
2021-12-17 14:01:12 -0500

    Add donation button

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { Provider } from '../query';
+import Script from 'next/script';
 import Theme from '@codeday/topo/Theme';
+import { Provider } from '../query';
 import 'react-responsive-modal/styles.css';
 
 export default function App ({ Component, pageProps }) {
   return (
-    <Theme withChat brandColor="red" analyticsId="AHHYLKBK">
-      <Provider value={pageProps?.query || {}}>
-        <Component {...pageProps} />
-      </Provider>
-    </Theme>
+    <>
+      <Script strategy="beforeInteractive" src="https://cdn.fundraiseup.com/widget/AHCSATYN" />
+      <Theme withChat brandColor="red" analyticsId="AHHYLKBK">
+        <Provider value={pageProps?.query || {}}>
+          <Component {...pageProps} />
+        </Provider>
+      </Theme>
+    </>
   );
 }
