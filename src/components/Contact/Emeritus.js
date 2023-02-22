ad7ceb24f62f244af7d810055553bd2c4fa8b0ca
Tyler Menezes <x@tyler.vc>
2023-02-21 17:43:28 -0800

    Add emeritus

---
+++ src/components/Contact/Emeritus.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Box, Grid, Text, Image } from '@codeday/topo/Atom';
+import { Content } from '@codeday/topo/Molecule';
+import shuffle from 'knuth-shuffle-seeded';
+import { useQuery } from '../../query';
+
+function EmeritusBox({ e }) {
+  return (
+    <Box>
+      <Image
+        src={e.picture.replace('256x256', 'w=64;h=64;fit=crop').replace('s=480', 's=32')}
+        rounded="full"
+        width="32px"
+        alt=""
+        float="left"
+        mr={2}
+        mt={1}
+      />
+      <Box>
+        <Text
+          mb={0}
+          whiteSpace="nowrap"
+          overflow="hidden"
+          textOverflow="ellipsis"
+        >
+          {e.name}
+        </Text>
+        <Text
+          fontSize="sm"
+          color="current.textLight"
+          whiteSpace="nowrap"
+          overflow="hidden"
+          textOverflow="ellipsis"
+        >
+          Emeritus
+        </Text>
+      </Box>
+    </Box>
+  );
+}
+
+export default function Emeritus({ seed, ...props }) {
+  const { account: { employees, otherTeam, board, emeritus } } = useQuery();
+  const employeeIds = [...employees, ...otherTeam, ...board].map((e) => e.id);
+  const all = shuffle(emeritus.filter((v) => !employeeIds.includes(v.id)), seed);
+
+  return (
+    <Content {...props}>
+      <Grid
+        templateColumns={{
+          base: 'repeat(2, minmax(0, 1fr))', lg: 'repeat(4, minmax(0, 1fr))',
+        }}
+        columnGap={8}
+        rowGap={1}
+      >
+        {all.map((e) => <EmeritusBox e={e} />)}
+      </Grid>
+    </Content>
+  );
+}
