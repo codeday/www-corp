8a8defbc97e4ee28f9f8646158707afb8aae94e2
Lola <lola@egherman.com>
2023-11-29 16:20:40 -0600

    fix possible bug when manually specifying volunteer region

--- src/pages/volunteer/[region].js
+++ src/pages/volunteer/[region].js
@@ -1,17 +1,19 @@
 import { print } from 'graphql';
 import { apiFetch } from '@codeday/topo/utils';
-import { VolunteerQuery, AllRegionsQuery } from './volunteer.gql'
+import { DateTime } from 'luxon';
+import { VolunteerQuery, AllRegionsQuery } from './volunteer.gql';
+
 export { default } from './index.js';
 
 export async function getStaticPaths() {
   return {
     paths: [],
     fallback: 'blocking',
-  }
+  };
 }
 
 export async function getStaticProps({ params: { region } }) {
-  const query = await apiFetch(print(VolunteerQuery), { now: new Date() })
+  const query = await apiFetch(print(VolunteerQuery), { now: DateTime.now().minus({ months: 6 }) });
 
   return {
     props: {
@@ -19,8 +21,8 @@ export async function getStaticProps({ params: { region } }) {
       seed: Math.random(),
       startBackground: 'student',
       startRegion: region,
-      startPage: 2
+      startPage: 2,
     },
     revalidate: 300,
-  }
+  };
 }
--- src/pages/volunteer/labs.js
+++ src/pages/volunteer/labs.js
@@ -1,19 +1,20 @@
 import { print } from 'graphql';
 import { apiFetch } from '@codeday/topo/utils';
-import { VolunteerQuery } from './volunteer.gql'
+import { DateTime } from 'luxon';
+import { VolunteerQuery } from './volunteer.gql';
+
 export { default } from './index.js';
 
-
 export async function getStaticProps() {
-  const query = await apiFetch(print(VolunteerQuery), { now: new Date() })
+  const query = await apiFetch(print(VolunteerQuery), { now: DateTime.now().minus({ months: 6 }) });
   return {
     props: {
       query,
       seed: Math.random(),
       startBackground: 'industry',
       startPage: 2,
-      layout: 'go'
+      layout: 'go',
     },
     revalidate: 300,
-  }
+  };
 }
